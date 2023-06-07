498ef910e385088178d17436e0b02e9e76a53b3d
Danilo <daanrod93@gmail.com>
2023-06-07 18:28:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# DISCORD BOT CHANNEL CLEANER
+
+Bot for clean discord channels
+
+Command example:
+```
+!clear 100
+```
+
+Start bot:
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+
+TOKEN = "YOUR DISCORD BOT TOKEN"
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.messages = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print("Bot connected")
+
+
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: int = 10):
+    try:
+        await ctx.channel.purge(limit=amount)
+        await ctx.send(f'Deleted {amount} messages.', delete_after=5)
+    except:
+        await ctx.send(f'Determine the amount msg to delete. "!clear 10"', delete_after=10)
+
+bot.run(TOKEN)
